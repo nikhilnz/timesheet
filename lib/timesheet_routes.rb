de775e1dc72112b545e4edf536ac294d113be1c4
nikhilnz <nikhilsharma@hotmail.com>
2014-06-25 07:50:37 +0100

    choosing date from drop down

--- lib/timesheet_routes.rb
+++ lib/timesheet_routes.rb
@@ -11,30 +11,43 @@ get '/name' do
   slim :'main'
 end
 
+get '/timesheet/dates' do
+  slim :'dates'
+end
+
+get '/timesheet/generate' do
+  month = params['month']
+  year = params['year'].to_i
+  puts month
+  puts year
+  generate_timesheet(month, year)
+
+end
+
+
 post '/timesheet/submit' do
 
-  month = Date::MONTHNAMES[Time.now.mon]
-  year = Time.now.year
+end
 
+def generate_timesheet(month, year)
   date_wrapper = CalendarHelper::DateWrapper.new(month, year)
 
-  from = "#{year}-#{Time.now.mon}-#{date_wrapper.first_day_of_month}"
-  to = "#{year}-#{Time.now.mon}-#{date_wrapper.last_day_of_month}"
+  from = "#{year}-#{month}-#{date_wrapper.first_day_of_month}"
+  to = "#{year}-#{month}-#{date_wrapper.last_day_of_month}"
 
-  pub_hols = HTTParty.get('http://localhost:4567/holidays/public', :query => { :from => from, :to => to}).to_i
+  pub_hols = HTTParty.get('http://localhost:4567/holidays/public', :query => {:from => from, :to => to}).to_i
   hols_taken = HTTParty.get('http://localhost:4567/holidays/personal').to_i
 
   total_days = date_wrapper.total_days
   weekends = date_wrapper.weekends
 
   working_days = total_days - weekends - pub_hols - hols_taken
-  "The month is #{month} and the year is #{year}. \n
+  "The month is #{month.capitalize} and the year is #{year}. \n
 Total number of days: #{total_days}. \n
 The number of public holidays are #{pub_hols}. \n
 The number of weekends are #{weekends}. \n
 Number of holidays taken: #{hols_taken} \n
 Total number of working days: #{working_days}"
 end
-
 
 
