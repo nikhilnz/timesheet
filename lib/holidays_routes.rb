c2df4bb0bdc385ac7f156888e2ccdb16442dd2c4
nikhilnz <nikhilsharma@hotmail.com>
2014-06-26 19:44:07 +0100

    start of unit testing end point

--- lib/holidays_routes.rb
+++ lib/holidays_routes.rb
@@ -8,13 +8,17 @@ configure do
   MongoMapper.setup({'production' => {'uri' => 'mongodb:/pub_hols'}}, 'production')
 end
 
+get '/holidays/public/view' do
+
+end
+
 get '/holidays/public' do
   from = Time.parse(params[:from])
   to = Time.parse(params[:to])
   puts from
   puts to
-  total_days = 0
   records = Holiday.where(:date => {:$gte => from}).where(:date => {:$lte => to})
+  total_days = 0
   records.each { |r| total_days = total_days + r.no_of_days.to_i }
   "#{total_days}"
 end
